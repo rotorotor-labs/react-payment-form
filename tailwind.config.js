352a5bad8321dc1ffd876553ba7768b45b80dab1
arrontp <arrontp@btinternet.com>
2022-08-19 12:07:40 +0100

    first upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,css,js}", "./public/**/*.{html,css,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
